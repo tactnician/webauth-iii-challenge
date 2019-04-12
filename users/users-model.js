6547e4b690cd7618c905083abcc2b2f47b25f7a6
tactnician <tactnician@gmail.com>
2019-04-11 22:49:55 -0400

    created user model

--- users/users-model.js
+++ users/users-model.js
@@ -1,1 +1,23 @@
-const db = require('')
+const db = require('../data/dbConfig');
+
+module.exports = {
+
+    add: async function(user) {
+        const [id] = await db('users').insert(user);
+        return this.findById(id);
+    },
+    find: function() {
+        return db('users').select('id', 'username', 'password');
+    },
+    findBy: function(filter) {
+        return db('users')
+            .where(filter)
+            .first();
+    },
+    findById: function(id) {
+        return db('users')
+        .where({ id })
+        .first();
+    }
+
+}
